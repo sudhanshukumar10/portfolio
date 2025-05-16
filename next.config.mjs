2ddf01d94411c1599c87934057577d52e689641a
SUDHANSHU KUMAR <sudhanshu.kumar@sunstone.in>
2025-05-16 19:05:54 +0530

    esd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+   assetPrefix: '/',
+};
